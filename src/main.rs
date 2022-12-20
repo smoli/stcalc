576dd4b39e6c2c629d2e18e21aaf506d68177420
Stephan Smola <smola@farbspender.de>
2022-12-20 14:16:43 +0100

    Repl mode

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,46 @@
+use std::io::stdin;
 use std::ops::Add;
 use crate::parser::Parser;
 use clap::Parser as Clapper;
+
 mod lexer;
 mod parser;
 
 #[derive(Clapper, Debug)]
-#[clap(author="Stephan Smola", version="0.1", about ="Unnecessary expression evaluator. It's one of many but this is mine.", long_about = None)]
+#[clap(author = "Stephan Smola", version = "0.1", about = "Unnecessary expression evaluator. It's one of many but this is mine.", long_about = None)]
 struct Args {
+    /// REPL mode
+    #[clap(short = 'r', long, action)]
+    repl: bool,
+
     /// Show only input
     #[clap(short, long, action)]
     input: bool,
 
     /// Show as equation
-    #[clap(short= 'q', long, action)]
+    #[clap(short = 'q', long, action)]
     equation: bool,
 
     expression: Vec<String>,
+
+}
+
+
+fn repl(firstExpression: String) {
+    loop {
+        let mut i = String::new();
+        match stdin().read_line(&mut i) {
+            Err(_) => return,
+
+            Ok(_) => {
+                let r = Parser::eval(i.as_str());
+                println!("{}", r);
+            }
+        }
+
+
+    }
+
 
 }
 
@@ -25,11 +50,18 @@ fn main() {
 
     let mut expression = String::from("");
     let mut sep = "";
+
     for e in args.expression {
         expression = expression.add(sep);
         expression = expression.add(e.as_str());
         sep = " ";
     }
+
+    if args.repl {
+        repl(expression);
+        return;
+    }
+
 
     if args.input {
         println!("{}", expression);
